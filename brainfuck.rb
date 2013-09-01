ed05725dc672f4ad7d74ae45d7fc70d776e0281c
urthbound <jeffowler@gmail.com>
2013-08-31 23:53:07 -0400

    works with hellp world

--- brainfuck.rb
+++ brainfuck.rb
@@ -1,6 +1,6 @@
 class String
   def to_bf
-    x = Brainfuck.new(self.scan(/[\[\].,><+-]/))
+    x = Brainfuck.new(self.split(//))
   end
 end
 
@@ -10,7 +10,7 @@ class Brainfuck
   def initialize(x = [])
     @prog = x  
     @ram = [] 
-    100.times do
+    10.times do
       @ram << 0
     end
 
@@ -31,12 +31,19 @@ class Brainfuck
     until @progindex == @prog.length 
       case @prog[@progindex] 
         when "<" then @pointer -= 1 
+          if @ram[@pointer] == nil
+            @ram.unshift(0)
+            @pointer += 1
+          end
+
         when ">" then @pointer += 1
+          if @ram[@pointer] == nil
+            @ram << 0
+          end
         when "." then print @ram[@pointer].chr
         when "," then @ram[@pointer] = gets.slice(0).ord
-        when "+" then @ram[@pointer] = (@ram[@pointer] + 1) 
-        when "-" then @ram[@pointer] = (@ram[@pointer] - 1)
-
+        when "+" then @ram[@pointer] += 1 
+        when "-" then @ram[@pointer] -=  1
         when "[" then
           if @ram[@pointer] == 0 
             @progindex += 1 until @prog[@progindex] == "]"
@@ -46,18 +53,37 @@ class Brainfuck
             @progindex -= 1 until @prog[@progindex] == "[" 
           end
       end
+
+
+        if @ram[@pointer] > 255
+          @ram[@pointer] -= 256
+        end
+
+        if @ram[@pointer] < 0
+          @ram[@pointer] += 256
+        end
+
+    #puts @ram.join(" ")
+   # puts @prog[@pointer]
+
+sleep 0.002
     @progindex += 1
     end
-   @progindex = 0
+
    self
   end
 end
 
-x=
-"
-++++++++++[>++++++++++<-]>.
-".to_bf
+x = "
+>+++++++++[<++++++++>-]<.>+++++++[<++++>-]<+.+++++++..+++.>>>++++++++[<++++>-]
+<.>>>++++++++++[<+++++++++>-]<---.<<<<.+++.------.--------.>>+.".to_bf
+
+system("clear")
 
 
-
+x.show
+puts puts puts
 x.run
+puts puts puts puts
+x.show
+puts x.prog.length
